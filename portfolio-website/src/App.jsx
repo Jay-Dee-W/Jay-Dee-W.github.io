4f4e43e1b250af580c81c7173f60a6c995ae8dbc
Jay-Dee-W <jaydee.wilson@gmail.com>
2025-04-24 23:10:19 +0200

    use vite & add hero comp

---
+++ portfolio-website/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+export default function PortfolioHome() {
+  return (
+    <main className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 p-8">
+      <Hero />
+    </main>
+  );
+}
